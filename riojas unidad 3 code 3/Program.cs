1829b5ca09c452baf4a5afab02169cbea2fe38b7
angel <angel@LAPTOP-TUOF9OKV>
2024-02-27 22:11:11 -0600

    Agregar archivos de proyecto.

---
+++ riojas unidad 3 code 3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class SimpleUser
+{
+    private string username;
+    private int age;
+
+    // Parameterized constructor
+    public SimpleUser(string name, int userAge)
+    {
+        username = name;
+        age = userAge;
+        Console.WriteLine($"User created: Username={username}, Age={age}");
+    }
+
+    // Method to greet the user
+    public void GreetUser()
+    {
+        Console.WriteLine($"Hello, {username}! You are {age} years old.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Create an instance of SimpleUser
+        SimpleUser user = new SimpleUser("John", 25);
+
+        // Greet the user
+        user.GreetUser();
+    }
+}
